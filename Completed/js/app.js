1ef265d94173ed5c62f786d047418e54dca3c658
jkinsfat <jkinsfat@w.edu>
2015-11-21 22:44:57 -0800

    Youtube videos embedded

--- Completed/js/app.js
+++ Completed/js/app.js
@@ -10,14 +10,14 @@ $(document).ready(function(){
         		"name": "Nyan Cat",
                 "description": "Animated cat with PopTart body, flying through space with a trailing rainbow",
                 "profilePic": "https://upload.wikimedia.org/wikipedia/en/e/ed/Nyan_cat_250px_frame.PNG",
-                "video": "https://www.youtube.com/watch?v=QH2-TGUlwu4"
+                "video": "https://www.youtube.com/embed/QH2-TGUlwu4"
             },
 
             {
         		"name": "Grumpy Cat",
                 "description": "Grumpy Cat became an internet sensation after her photo was posted on Reddit on September 22, 2012. It was suggested that the original photo was photoshopped, so we posted a few videos on YouTube. The videos went viral and her popularity has continued to increase!",
                 "profilePic": "http://cdn2.thr.com/sites/default/files/imagecache/675x380/2014/09/too_good_for_grumpy_cat.jpg",
-                "catImage": {
+                "catImage": 
                      "image": "http://cdn.grumpycats.com/wp-content/uploads/2012/10/I-Had-Fun-Once-It-Was-Awful.jpeg",
                      "image": "http://vignette4.wikia.nocookie.net/lego/images/c/c3/39215-grumpy-cat-no-Rwoe.jpeg/revision/latest?cb=20130709214607",
                      "image": "http://i4.mirror.co.uk/incoming/article4764747.ece/ALTERNATES/s615/Grumpy-Cat.jpg"
@@ -28,7 +28,7 @@ $(document).ready(function(){
         		"name": "Keyboard Cat",
                 "description": "Keyboard Cat is an Internet meme. It consists of a video from 1984 of a cat called 'Fatso' wearing a blue shirt and 'playing' an upbeat rhythm on an electronic keyboard. The video was posted to YouTube under the title 'charlie schmidt's cool cats' in June 2007.",
             	"profilePic": "http://img1.wikia.nocookie.net/__cb20140726152323/smashbroslawlorigins/images/3/36/KEYBOARD_CAT.png",
-            	"video": "https://www.youtube.com/watch?v=J---aiyznGQ"
+            	"video": "https://www.youtube.com/embed/J---aiyznGQ"
 
             },
 
