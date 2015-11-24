3fd348a3078fa5edcb3e90786cfb3bf6a3e2f5e3
Meera Limaye <mlimaye@uw.edu>
2015-11-23 21:47:32 -0800

    tutorial complete

--- Starter/js/app.js
+++ Starter/js/app.js
@@ -1,11 +1,14 @@
-// Completed JS
 'use strict';
-
 $(document).ready(function(){
 
-    {
+  var templateSource = $('#entry-template').html();
+  var template = Handlebars.compile(templateSource);
+  var context = {"title":"CelebriKitties",
+                "cat": [
+
+            {
             "name": "Nyan Cat",
-                "description": "Animated cat with PopTart body, flying through space with a trailing rainbow",
+                "description": "Nyan Cat, also known as Pop Tart Cat, is an 8-bit animation depicting a cat with the body of a cherry pop tart flying through outer space. While absurd themes like flying kittens and pastry cats have been around for some time, the surreal humor behind this particular combination has captivated YouTubers and online art communities, spawning fan illustrations as well as user-interface designs and homebrew games across multiple platforms.",
                 "profilePic": "https://upload.wikimedia.org/wikipedia/en/e/ed/Nyan_cat_250px_frame.PNG",
                 "video": "https://www.youtube.com/embed/QH2-TGUlwu4"
             },
@@ -13,7 +16,7 @@ $(document).ready(function(){
             {
             "name": "Grumpy Cat",
                 "description": "Grumpy Cat became an internet sensation after her photo was posted on Reddit on September 22, 2012. It was suggested that the original photo was photoshopped, so we posted a few videos on YouTube. The videos went viral and her popularity has continued to increase!",
-                "profilePic": "http://cdn2.thr.com/sites/default/files/imagecache/675x380/2014/09/too_good_for_grumpy_cat.jpg",
+                "profilePic": "http://images.nymag.com/news/business/boom-brands/business130930_grumpycat_2_560.jpg",
                 "catImage": "http://cdn.grumpycats.com/wp-content/uploads/2012/10/I-Had-Fun-Once-It-Was-Awful.jpeg"                  
             },
 
@@ -28,9 +31,12 @@ $(document).ready(function(){
             {
             "name": "Lil Bub",
                 "description": "BUB was born with a multitude of genetic anomalies which all add up to one of nature's happiest accidents. She is a 'perma-kitten', which means she will stay kitten sized and maintain kitten-like features her entire life. She also has an extreme case of dwarfism, which means her limbs are disproportionately small relative to the rest of her body and she has some difficulty moving around.",
-              "profilePic": "http://cdn.thedailybeast.com/content/dailybeast/articles/2013/04/19/lil-bub-the-world-s-cutest-cat-stars-in-documentary-lil-bub-friendz-at-tribeca/jcr:content/image.img.2000.jpg/1400303393447.cached.jpg",
+              "profilePic": "http://m1.behance.net/rendition/modules/90497669/disp/a79df67ace41158af5025ad53f967260.jpg",
             "catImage": "https://upload.wikimedia.org/wikipedia/commons/3/3f/Lil_Bub_2013_(crop_for_thumb).jpg"
           }]
         };
 
+  var html    = template(context);  
+  
+  $(html).appendTo('#content');
 });
